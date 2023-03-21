8779041c4a9455f93f7d972766000f3395d0748e
oliverLo78 <oliverberto@gmail.com>
2023-03-21 17:03:25 -0400

    so now user can have many wines basically a list of favorites

--- models/index.js
+++ models/index.js
@@ -2,6 +2,18 @@ const User = require('./User');
 const Review = require('./Review');
 const Wine = require('./Wine');
 
+// Define a User as having many Wines, thus creating a foreign key in the 
+// 'wine' table
+User.hasMany(Wine, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
+
+// The association can also be created from the Wine side
+Wine.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
 User.belongsToMany(Review, {
   // Define the third table needed to store the foreign keys
   through: {
