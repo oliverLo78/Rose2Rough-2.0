ae410d9622edb17d3b576324ff64b17b7d11fbbd
oliverLo78 <oliverberto@gmail.com>
2023-03-28 14:50:27 -0400

    added review, reviewList routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const reviewRoutes = require('../reviewRoutes');
+const reviewlistRoutes = require('./reviewlistRoutes');
 
 router.use('/users', userRoutes);
+router.use('/review', reviewRoutes);
+router.use('/reviewList', reviewlistRoutes);
 
 module.exports = router;
