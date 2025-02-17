156c17ba5ee8b935d36dc249e9cd0f6fda9c0196
Oliver Lopez <oliverberto@gmail.com>
2025-02-17 17:08:15 -0500

    Add debug log for JAWSDB_URL

--- config/connection.js
+++ config/connection.js
@@ -4,8 +4,10 @@ require('dotenv').config();
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
+  console.log('Using JAWSDB_URL for database connection:', process.env.JAWSDB_URL);
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
+  console.log('Using local database configuration');
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: 'localhost',
     dialect: 'mysql',
@@ -17,4 +19,3 @@ if (process.env.JAWSDB_URL) {
 }
 
 module.exports = sequelize;
-
